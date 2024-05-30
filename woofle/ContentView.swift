aa5b489012c0f586b14b40127c85077624efe8f8
Riyanpahuja04 <riyan.pahuja4@gmail.com>
2024-05-30 11:09:42 +1000

    Initial Commit

---
+++ woofle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  woofle
+//
+//  Created by Riyan Pahuja on 30/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ woofle/woofleApp.swift
@@ -0,0 +1,17 @@
+//
+//  woofleApp.swift
+//  woofle
+//
+//  Created by Riyan Pahuja on 30/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct woofleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
