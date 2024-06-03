588af3de909530aca592bad06d32fb8e0a7d5ff8
Riyanpahuja04 <riyan.pahuja4@gmail.com>
2024-06-03 14:14:10 +1000

    static ui for activity page.

---
+++ woofle/View/activity/ActivitySelection.swift
@@ -0,0 +1,18 @@
+//
+//  ActivitySelection.swift
+//  woofle
+//
+//  Created by Riyan Pahuja on 3/6/2024.
+//
+
+import SwiftUI
+
+struct ActivitySelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivitySelection()
+}
