f7bca717fb970e2a582111702b81475a165e44c8
uts-james-handley <james.t.handley@student.uts.edu.au>
2024-06-04 14:01:56 +1000

    Input Goal Page and Help Screen
    Complete Page with Help Screen

--- woofle/goalInputScreen.swift
+++ woofle/goalInputScreen.swift
@@ -9,65 +9,204 @@ import SwiftUI
 
 struct goalInputScreen: View {
     
-    @State private var userInputText: String = ""
     
-    var body: some View {
-        VStack {
-            Spacer()
+    // Temp colors until global color variables are made (just for my reference)
+    private let _grayColor: Color = (Color(red: 189/255, green: 189/255, blue: 189/255))
+    private let _orangeColor: Color = (Color(red: 255/255, green: 157/255, blue: 74/255))
+    private let _defaultBackgroundColor: Color = Color(red: 255/255, green: 253/255, blue: 248/255)
+    // END TEMP COLORS
+    
+    @State private var userInputText: String = ""
+    @State private var opacityMaskValue: Double = 1
+    @State private var backgroundMaskColor: Color = Color(red: 255/255, green: 253/255, blue: 248/255)
+    @State private var showHelp: Bool = false
+    
+    func currentButtonColor() -> Color {
+        if !userInputText.isEmpty && userInputText.count < 61 {
             
-            HStack {
-                Spacer()
-                Image(systemName: "questionmark.circle")
-                    .scaledToFit()
-                    .font(.largeTitle)
-                    .padding(.horizontal, 30)
-            }
+            return _orangeColor
             
-            Image("PLACEHOLDER_Woofle")
-                .resizable()
-                .scaledToFit()
-                .padding(.horizontal, 64)
+            // Function Call to Backend
+            // Networking with ChatGPT, if prompt is compatable, return true
             
-            Text("What is your Goal?")
-                .font(
-            Font.custom("SF Pro", size: 24)
-            .weight(.semibold))
-                .padding(.vertical, 15)
+            // If Return Value == True
+            // Switch to next page
+            // Else; Try Again and show Error
             
-            HStack {
-            
-                Spacer(minLength: 75)
-                    
-                
-                TextField("Please type in your goal...", text: $userInputText)
-                    .textFieldStyle(.roundedBorder)
-                    .lineLimit(4, reservesSpace: true)
-                
-                
-                
-                
-                Image(systemName: "dice.fill")
-                    .scaledToFit()
-                    .font(.title)
-                    .padding(.trailing, 30.0)
-                
-                
-            }
-            
-            
-            Rectangle()
-            
-            Button("Submit")
-            {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }
-            .padding()
-
-                
+        }
+        
+        else {
+            return _grayColor
+        }
+    }
+    
+    func characterCounterColor() -> Color {
+        
+        if userInputText.count > 60 {
+            return .red
+        }
+        
+        else {
+            return .black
         }
         
     }
+    
+    func enableBackgroundMask() {
+        backgroundMaskColor = _grayColor
+        opacityMaskValue = 0.19
+    }
+    
+    func disableBackgroundMask() {
+        backgroundMaskColor = _defaultBackgroundColor
+        opacityMaskValue = 1
+    }
+    
+    var body: some View {
+        
+        ZStack {
+            
+            VStack {
+                
+                Spacer().frame(minHeight: 0, maxHeight: 40)
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "questionmark.circle")
+                        .scaledToFit()
+                        .font(.largeTitle)
+                        .padding(.horizontal, 30)
+                        .onTapGesture {
+                            if !showHelp {
+                                enableBackgroundMask()
+                                showHelp.toggle()
+                            }
+                        }
+                }
+                
+                Image("woofle 1")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal, 64)
+                
+                
+                Text("What is your Goal?")
+                    .font(
+                        Font.custom("SF Pro", size: 24)
+                            .weight(.semibold))
+                    .padding(.vertical, 15)
+                
+                
+                HStack { // Contains Input and Dice Button
+                    
+                    Spacer(minLength: 80)
+                    
+                    
+                    TextField("Please type in your goal...", text: $userInputText, axis: .vertical)
+                        .textFieldStyle(.roundedBorder)
+                        .lineLimit(4)
+                    
+                    Image(systemName: "dice.fill")
+                        .scaledToFit()
+                        .font(.title)
+                        .padding(.trailing, 30.0)
+                        .padding(.leading, 5)
+                    
+                    
+                } // END Text Input and Dice Button HStack
+                
+                HStack {
+                    
+                    Spacer(minLength: 80) // Same spacer as Input Box to ensure they always line up
+                    
+                    Text("\(userInputText.count)/60").font(.caption).padding(.trailing, 85.0)
+                        .foregroundColor(characterCounterColor())
+                    
+                }
+                
+                
+                Spacer()
+                
+                HStack { // Submit Button and Spacing
+                    
+                    Spacer().frame(minWidth: 0, maxWidth: 25)
+                    
+                    Button("Submit")
+                    {
+                        
+                        if !userInputText.isEmpty {
+                            
+                            // Function Call to Backend
+                            // Networking with ChatGPT, if prompt is compatable, return true
+                            
+                            // If Return Value == True
+                            // Switch to next page
+                            // Else; Try Again and show Error
+                            
+                        }
+
+                    }
+                    .frame(minWidth: 160, maxWidth: .infinity)
+                    .padding(.horizontal, 25)
+                    .padding(.vertical, 14)
+                    .foregroundColor(.white)
+                    .background(currentButtonColor())
+                    .font(
+                        Font.custom("SF Pro", size: 16)
+                            .weight(.semibold))
+                    .cornerRadius(8)
+                    .scaledToFit()
+                    
+                    Spacer().frame(minWidth: 0, maxWidth: 25)
+                    
+                } // END Submit Button HStack
+                
+            }
+            // Opacity Mask for Info Menu
+            .mask(Rectangle().opacity(opacityMaskValue))
+            // Background colour for the page
+            .background(backgroundMaskColor)
+            
+            if showHelp {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .padding(.horizontal, 60)
+                        .padding(.vertical, 220)
+                        .foregroundColor(.white)
+                        .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+                        
+                        
+                    VStack {
+                        Text("What you want to discover?").font(.system(size: CGFloat(18))).bold()
+                            .foregroundColor(Color(red: 0.16, green: 0.16, blue: 0.21))
+                            .padding(.vertical, 10)
+                        
+                        Text("What are the things you have wanted to try for so long but have not put into practice? \nWe encourage you to log in your goal and discover new experience by stepping out of the comfort zone. \nEnjoy your discovery journey!")
+                            .font(.system(size: CGFloat(16)))
+                            .foregroundColor(Color(red: 0.16, green: 0.16, blue: 0.21))
+                            .padding(.horizontal, 80)
+                        
+                        Button("Got It!") {
+                            disableBackgroundMask()
+                            showHelp.toggle()
+                        }
+                        .foregroundColor(.white)
+                        .bold()
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 40)
+                        .background(_orangeColor)
+                        .cornerRadius(10)
+                        .padding()
+                    }
+                    
+                }.animation(.easeIn, value: showHelp)
+            }
+        }
+        
+    }
+    
 }
+
 
 #Preview {
     goalInputScreen()
