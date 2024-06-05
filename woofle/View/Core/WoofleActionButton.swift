24ef53bbb347646e712492c6a9b8a628dc2004ab
Riyanpahuja04 <riyan.pahuja4@gmail.com>
2024-06-05 10:56:03 +1000

    cleaned up code

---
+++ woofle/View/Core/WoofleActionButton.swift
@@ -0,0 +1,18 @@
+//
+//  WoofleButton.swift
+//  woofle
+//
+//  Created by Riyan Pahuja on 5/6/2024.
+//
+
+import SwiftUI
+
+struct WoofleActionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WoofleActionButton()
+}
