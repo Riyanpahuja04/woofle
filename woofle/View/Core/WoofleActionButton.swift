e047aa1342d716d0769e03610a6059026ae3dc68
Riyanpahuja04 <riyan.pahuja4@gmail.com>
2024-06-05 11:06:31 +1000

    resolved conflicts

--- woofle/View/Core/WoofleActionButton.swift
+++ woofle/View/Core/WoofleActionButton.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct WoofleActionButton: View {
+    var text: String
+    var action: () -> Void
+    
+    private let _buttonColor = Color(red: 0.15, green: 0.16, blue: 0.21)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: action) {
+            Text("Submit")
+                .font(
+                    Font.system(size: 16)
+                )
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 14)
+                .padding(.horizontal, 25)
+                .background(Color(red: 1, green: 0.62, blue: 0.29))
+                .foregroundStyle(_buttonColor)
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .shadow(color: .black.opacity(0.05), radius: 1, x: 0, y: 1)
+        .padding(.horizontal, 25)
     }
 }
 
 #Preview {
-    WoofleActionButton()
+    WoofleActionButton(text: "Add Text Here", action: {})
 }
--- woofle/View/activity/ActivitySelection.swift
+++ woofle/View/activity/ActivitySelection.swift
@@ -3,109 +3,96 @@ import SwiftUI
 struct ActivitySelection: View {
     @State private var selectedTask: String?
     @State private var isSpinning: Bool = false
+    
+    private let _titleColor = Color(red: 0.28, green: 0.29, blue: 0.43)
+    private let _subTitleColor = Color(red: 1, green: 0.99, blue: 0.95)
+    private let _captionColor = Color(red: 0.09, green: 0.09, blue: 0.09)
+    private let _linkColor = Color(red: 0.43, green: 0.6, blue: 0.59)
+    
     // TODO: fetch api data to show
     var body: some View {
-        ScrollView {
-            ZStack {
-                Image("TopBgActivity")
-                    .resizable()
-                    .scaledToFill()
-                
-                VStack(spacing: 5) {
-                    // TODO: populate data from api
-                    VStack(alignment:.leading, spacing: 7) {
-                        Text("Playing “Stand By Me” Using Basic Chords On Guitar")
-                            .font(.system(size: 20))
-                            .fontWeight(.medium)
-                            .foregroundStyle(Color(red: 1, green: 0.99, blue: 0.95))
-                        
-                        
+            ScrollView {
+                ZStack {
+                    Image("TopBgActivity")
+                        .resizable()
+                        .scaledToFill()
+                    
+                    VStack(spacing: 5) {
                         // TODO: populate data from api
-                        Text("What activity you want to do?")
-                            .font(.system(size: 35))
-                            .fontWeight(.semibold)
-                            .foregroundColor(Color(red: 0.28, green: 0.29, blue: 0.43))
-                    }
-                    .padding(.horizontal, 60)
-                    
-                    
-                    Spacer()
-                    
-                    VStack {
-                        Text("LEVEL 0")
-                            .font(.system(size: 24)
-                            )
-                            .fontWeight(.semibold)
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(Color(red: 0.28, green: 0.29, blue: 0.43))
-                            .padding(.bottom, 30)
-                        ForEach(sample.indices, id: \.self) { index in
-                            ActivitySelectionButton(
-                                task: sample[index]["task"]!,
-                                description: sample[index]["description"]!,
-                                selectedTask: $selectedTask
-                            )
+                        VStack(alignment:.leading, spacing: 7) {
+                            Text("Playing “Stand By Me” Using Basic Chords On Guitar")
+                                .font(.system(size: 20))
+                                .fontWeight(.medium)
+                                .foregroundStyle(_subTitleColor)
+                            
+                            
+                            Text("What activity you want to do?")
+                                .font(.system(size: 35))
+                                .fontWeight(.semibold)
+                                .foregroundColor(_titleColor)
                         }
-                    }
-                    
-                    Spacer()
-                    
-                    HStack(spacing: 2) {
-                        Text("Don’t like these options?")
-                            .font(.system(size: 15))
-                            .multilineTextAlignment(.center)
-                            .foregroundStyle(Color(red: 0.09, green: 0.09, blue: 0.09))
+                        .padding(.horizontal, 60)
                         
+                        Spacer()
                         
-                        Button(action: {
-                            isSpinning.toggle()
-                        }) {
-                            HStack(spacing: 2) {
-                                Text("Refresh now")
-                                    .font(
-                                        Font.custom("SF Pro", size: 15)
-                                            .weight(.medium)
-                                    )
-                                    .multilineTextAlignment(.center)
-                                    .foregroundStyle(Color(red: 0.43, green: 0.6, blue: 0.59))
-                                
-                                Image(systemName: "arrow.triangle.2.circlepath")
-                                    .foregroundStyle(Color(red: 0.43, green: 0.6, blue: 0.59))
-                                    .rotationEffect(.degrees(isSpinning ? 360 : 0))
-                                    .animation(Animation.linear(duration: 1).repeatCount(1, autoreverses: false), value: isSpinning)
-                                
+                        VStack {
+                            Text("LEVEL 0")
+                                .font(.system(size: 24)
+                                )
+                                .fontWeight(.semibold)
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(_titleColor)
+                                .padding(.bottom, 30)
+                            ForEach(_sample.indices, id: \.self) { index in
+                                ActivitySelectionButton(
+                                    task: _sample[index]["task"]!,
+                                    description: _sample[index]["description"]!,
+                                    selectedTask: $selectedTask
+                                )
+                            }
+                        }
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 2) {
+                            Text("Don’t like these options?")
+                                .font(.system(size: 15))
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(_captionColor)
+                            
+                            
+                            Button(action: {
+                                isSpinning.toggle()
+                            }) {
+                                HStack(spacing: 2) {
+                                    Text("Refresh now")
+                                        .font(Font.system(size: 15))
+                                        .fontWeight(.medium)
+                                        .multilineTextAlignment(.center)
+                                        .foregroundStyle(_linkColor)
+                                    
+                                    Image(systemName: "arrow.triangle.2.circlepath")
+                                        .foregroundStyle(_linkColor)
+                                        .rotationEffect(.degrees(isSpinning ? 360 : 0))
+                                        .animation(Animation.linear(duration: 1).repeatCount(1, autoreverses: false), value: isSpinning)
+                                    
+                                    
+                                }
                                 
                             }
-                            
                         }
-                    }
-                    .padding(.bottom, 30)
-                    
-                    Button(action: {}) {
+                        .padding(.bottom, 30)
+                        
                         // TODO: Add tappable functionality
-                        Text("Submit")
-                            .font(
-                                Font.custom("SF Pro", size: 16)
-                                    .weight(.semibold)
-                            )
-                            .foregroundStyle(.white)
-                            .frame(maxWidth: .infinity)
-                            .padding(.vertical, 14)
-                            .padding(.horizontal, 25)
-                            .background(Color(red: 1, green: 0.62, blue: 0.29))
-                            .foregroundStyle(Color(red: 40/255, green: 42/255, blue: 55/255))
+                        WoofleActionButton(text: "Submit", action: {})
+                        
+                        Spacer()
                     }
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .shadow(color: .black.opacity(0.05), radius: 1, x: 0, y: 1)
-                    .padding(.horizontal, 25)
-                    
-                    Spacer()
+                    .safeAreaPadding(.top, 68)
                 }
-                .safeAreaPadding(.top, 68)
+                .ignoresSafeArea()
             }
             .ignoresSafeArea()
-        }
-        .ignoresSafeArea()
     }
 }
 
@@ -114,7 +101,7 @@ struct ActivitySelection: View {
 }
 
 // TODO: Delete this once API is ready
-let sample = [
+let _sample = [
     [
         "task": "Learn Basic Chords",
         "description": "The four most common guitar chords are E-Minor, C, G and D. Youtube Tutorials would be a good place to start!"
