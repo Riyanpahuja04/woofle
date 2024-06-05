1450d1e1e11526d150af6f675b9115616d7bab60
Riyanpahuja04 <riyan.pahuja4@gmail.com>
2024-06-05 11:02:44 +1000

    resolved conflicts

---
+++ woofle/Model/SampleSwiftModel.swift
@@ -0,0 +1,12 @@
+//
+//  SampleSwiftModel.swift
+//  woofle
+//
+//  Created by Riyan Pahuja on 3/6/2024.
+//
+
+/*
+ In iOS development, a model class represents the data and business logic of an application. It encapsulates the application's core data and defines the structure of the data being used. In SwiftUI, model classes are often plain Swift classes or structs that hold the data. They can be simple data containers or more complex entities that manage data operations, such as fetching from a network or a database.
+ */
+
+import Foundation
