2cb8d04af9b5686c9edc3b966e44871b13999e3a
uts-james-handley <james.t.handley@student.uts.edu.au>
2024-06-11 15:21:30 +1000

    Feature/integration (#17)
    * Added DropDown view model and animation integration
    
    * Reorganised Map View Folder Structure
    
    * Minor Position Changes

---
+++ woofle/View/Level/activitySelectedScreen.swift
@@ -0,0 +1,65 @@
+//
+//  activitySelectedScreen.swift
+//  woofle
+//
+//  Created by James Handley on 6/6/2024.
+//
+
+import SwiftUI
+
+struct activitySelectedScreen: View {
+    
+    @ObservedObject var dropdownViewModel = DropdownViewModel()
+    @State private var storesView: AnyView?
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+            
+            MapView()
+            
+            VStack {
+                
+                Rectangle().ignoresSafeArea().frame(width: .infinity, height: 0).foregroundColor(_defaultBackgroundColor)
+                
+                switch dropdownViewModel.currentMenu {
+                case 0:
+                    _levelDropdown
+                case 1:
+                    _levelDropdownExpanded
+                case 2:
+                    _levelDropdownNewActivity
+                case 3:
+                    _levelDropdownNoActivity
+                default:
+                    _levelDropdown
+                }
+                
+            }
+            
+        }
+    }
+    
+    var _levelDropdown: some View {LevelDropdown(dropdownViewModel: dropdownViewModel)}
+    var _levelDropdownExpanded: some View {LevelDropdownExpanded(dropdownViewModel: dropdownViewModel)}
+    var _levelDropdownNewActivity: some View {LevelDropdownNewActivity(dropdownViewModel: dropdownViewModel)}
+    var _levelDropdownNoActivity: some View {LevelDropdownNoActivity(dropdownViewModel: dropdownViewModel)}
+    
+    
+    // Temp colors until global color variables are made (just for my reference)
+    private let _grayColor: Color = (Color(red: 189/255, green: 189/255, blue: 189/255))
+    private let _orangeColor: Color = (Color(red: 255/255, green: 157/255, blue: 74/255))
+    private let _defaultBackgroundColor: Color = Color(red: 255/255, green: 253/255, blue: 248/255)
+    private let _buttonGrey: Color = Color(red: 225/255, green: 225/255, blue: 225/255)
+    private let _currentPageIndicatorColor: Color = Color(red: 75/255, green: 134/255, blue: 131/255)
+    private let _helpScreenTextColor: Color = Color(red: 0.16, green: 0.16, blue: 0.21)
+    private let _dropdownImageColor: Color = Color(red: 0.28, green: 0.29, blue: 0.43)
+    private let _greenHeaderColor: Color = Color(red: 0.43, green: 0.6, blue: 0.59)
+    private let _teal: Color = Color(red: 0.43, green: 0.6, blue: 0.59)
+    // END TEMP COLORS
+    
+    
+}
+
+#Preview {
+    activitySelectedScreen()
+}
