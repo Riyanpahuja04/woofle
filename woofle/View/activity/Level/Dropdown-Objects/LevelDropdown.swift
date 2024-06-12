d0cb3c5ef264fcbb4affba3fc126c39b52176e01
uts-james-handley <james.t.handley@student.uts.edu.au>
2024-06-12 14:23:30 +1000

    Revert "Past completion page (#32)"
    This reverts commit 1292cd5d7cd86e2bbeacbc41aa39b95d842de58b.

--- woofle/View/activity/Level/Dropdown-Objects/LevelDropdown.swift
+++ woofle/View/activity/Level/Dropdown-Objects/LevelDropdown.swift
@@ -14,81 +14,69 @@ struct LevelDropdown: View {
     var body: some View {
         
         ZStack {
-            
-            VStack {
                 
-                Rectangle()
-                    .foregroundColor(_defaultBackgroundColor)
-                    .cornerRadius(20)
-                    .frame(width: .infinity, height: 200)
-                Capsule()
-                    .trim(from: 0.05, to: 0.45)
-                    .foregroundColor(_defaultBackgroundColor)
-                    .frame(width: 140, height: 120)
-                    .padding(.vertical, -90)
-                    .onTapGesture{
-                        //TODO: CODE
-                    }
-            }
-            .shadow(radius:2, x: 0, y: 4)
-            VStack {
-                
-                Text("Level: \(GlobalActivityTracker.shared.level)")
-                    .foregroundColor(_dropdownImageColor)
-                    .font(
-                        .system(size: 20)
-                        .weight(.bold)
-                    )
-                    .multilineTextAlignment(.center)
-                    .padding(.top, -5)
-                    .padding(.bottom, 10)
-                
-                Text(GlobalActivityTracker.shared.currentGoal)
-                    .foregroundColor(_dropdownImageColor)
-                    .font(
-                        .system(size: 20)
-                        .weight(.semibold)
-                    )
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal, 20)
-                
-                Text(GlobalActivityTracker.shared.selectedOption?.brief ?? "Infinity")
-                    .frame(width: .infinity, height: 40)
-                    .font(
-                        .system(size: 20)
-                        .weight(.medium)
-                    )
-                    .padding(.horizontal, 60)
-                    .background(_greenHeaderColor)
-                    .cornerRadius(10)
-                    .foregroundColor(_defaultBackgroundColor)
-                
-            }
-            
-            Image(systemName: "chevron.down")
-                .font(.title)
-                .padding(.horizontal, 30)
-                .foregroundColor(_dropdownImageColor)
-                .padding(.top, 200 + 20)
-                .onTapGesture{
-                    withAnimation(.interpolatingSpring) {
-                        dropdownViewModel.currentMenu = 1
-                    }
+                VStack {
+                    
+                    Rectangle()
+                        .foregroundColor(_defaultBackgroundColor)
+                        .cornerRadius(20)
+                        .frame(width: .infinity, height: 200)
+                    Capsule()
+                        .trim(from: 0.05, to: 0.45)
+                        .foregroundColor(_defaultBackgroundColor)
+                        .frame(width: 140, height: 120)
+                        .padding(.vertical, -90)
+                        .onTapGesture{
+                            //TODO: CODE
+                        }
                 }
-            
-        }.padding(.top, -40)
-        ZStack {
-            Circle()
-                .frame(width: 80)
-                .foregroundColor(_defaultBackgroundColor)
                 .shadow(radius:2, x: 0, y: 4)
-            Image(systemName: "note.text")
-                .resizable()
-                .frame(width:40, height:40)
-                .foregroundColor(_dropdownImageColor)
+                VStack {
+                    
+                    Text("Level: \(GlobalActivityTracker.shared.level)")
+                        .foregroundColor(_dropdownImageColor)
+                        .font(
+                            .system(size: 20)
+                            .weight(.bold)
+                        )
+                        .multilineTextAlignment(.center)
+                        .padding(.top, -5)
+                        .padding(.bottom, 10)
+                    
+                    Text(GlobalActivityTracker.shared.currentGoal)
+                        .foregroundColor(_dropdownImageColor)
+                        .font(
+                            .system(size: 20)
+                            .weight(.semibold)
+                        )
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 20)
+                    
+                    Text(GlobalActivityTracker.shared.selectedOption?.brief ?? "Infinity")
+                        .frame(width: .infinity, height: 40)
+                        .font(
+                            .system(size: 20)
+                            .weight(.medium)
+                        )
+                        .padding(.horizontal, 60)
+                        .background(_greenHeaderColor)
+                        .cornerRadius(10)
+                        .foregroundColor(_defaultBackgroundColor)
+                    
+                }
                 
-        } .offset(x:130, y:40)
-            
+                Image(systemName: "chevron.down")
+                    .font(.title)
+                    .padding(.horizontal, 30)
+                    .foregroundColor(_dropdownImageColor)
+                    .padding(.top, 200 + 20)
+                    .onTapGesture{
+                        withAnimation(.interpolatingSpring) {
+                            dropdownViewModel.currentMenu = 1
+                        }
+                    }
+                
+            }.padding(.top, -40)
     }
     
     
--- woofle/View/activity/Level/Dropdown-Objects/LevelDropdownNoActivity.swift
+++ woofle/View/activity/Level/Dropdown-Objects/LevelDropdownNoActivity.swift
@@ -57,17 +57,6 @@ struct LevelDropdownNoActivity: View {
                 
         }
         .padding(.top, -40)
-        ZStack {
-            Circle()
-                .frame(width: 80)
-                .foregroundColor(_defaultBackgroundColor)
-                .shadow(radius:2, x: 0, y: 4)
-            Image(systemName: "note.text")
-                .resizable()
-                .frame(width:40, height:40)
-                .foregroundColor(_dropdownImageColor)
-                
-        } .offset(x:130, y:40)
     }
     
     
